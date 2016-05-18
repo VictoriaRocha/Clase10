bf389323f0ad572f34d040e6ea2bba691c0684f4
Alejandro Tocar <aletocar@hotmail.com>
2016-05-18 20:31:59 -0300

    fin clase 10

---
+++ Tresana/Tresana.Web.Api/app/app.js
@@ -0,0 +1,23 @@
+﻿(function() {
+    'use-strict';
+
+    var app = angular.module('Tresana', ['ngMessages', 'ngRoute']);
+
+    app.config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+
+        .when('/tasks', {
+            templateUrl: 'app/tasks/tasks.html',
+            controller: 'Tasks.Controller'
+        })
+        .when('/users', {
+            templateUrl: 'app/users/users.html',
+            controller: 'Users.Controller'
+        })
+        .otherwise({
+            redirectTo: '/tasks'
+        });
+    });
+
+
+})();
---
+++ Tresana/Tresana.Web.Api/app/tasks/tasksController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    var tresanaApp = angular.module('Tresana');
+
+    tresanaApp.controller('Tasks.Controller', function ($log, $http) {
+        var ctrl = this;
+
+        $http.get('/api/tasks')
+            .success(function (result) {
+                ctrl.tasks = result;
+            })
+        .error(function (data, status) {
+            $log.error(data);
+        });
+
+
+        ctrl.printDate = function printDate(dateString) {
+            var date = new Date(dateString)
+            return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " at " + date.getUTCHours() + ":" + date.getUTCMinutes();
+        }
+
+        console.log(ctrl);
+
+    });
+})();
